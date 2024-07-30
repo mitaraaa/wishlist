5e37e94c41b4db8cb92120049e2ed10577a9776c
mitaraaa <rustam030201@gmail.com>
2024-07-31 01:22:41 +0500

    chore: use `@/` imports and add prettier plugin for import sorting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: fileURLToPath(new URL("src/", import.meta.url)),
+        },
+      ],
+    },
+  };
+});
