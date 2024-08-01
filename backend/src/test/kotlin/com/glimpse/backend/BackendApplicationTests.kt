f3492c740bb80bab56c1397f04b10686d8a64ba6
mitaraaa <rustam030201@gmail.com>
2024-08-01 17:19:18 +0500

    chore: add base backend files

---
+++ backend/src/test/kotlin/com/glimpse/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glimpse.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
